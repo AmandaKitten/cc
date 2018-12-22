7ed9dacd351ca526a4811933582890b7fb413760
t00000685 <t00000685@vipshop.com>
2018-12-22 23:45:06 +0800

    页面导航完成

--- build/webpack.base.conf.js
+++ build/webpack.base.conf.js
@@ -34,8 +34,9 @@ module.exports = {
   resolve: {
     extensions: ['.js', '.vue', '.json'],
     alias: {
-      'src~': resolve('src'),
-      'common':resolve('src/common')
+      'src': resolve('src'),
+      'common':resolve('src/common'),
+      'components':resolve('src/components')
     }
   },
   module: {
--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,9 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
+import Recommend from 'components/recommend/recommend.vue'
+import Singer from 'components/singer/singer.vue'
+import Rank from 'components/rank/rank.vue'
+import Search from 'components/search/search.vue'
 
 Vue.use(Router)
 
@@ -9,7 +12,27 @@ export default new Router({
     {
       path: '/',
       name: 'HelloWorld',
-      component: HelloWorld
+      component: Recommend
+    },
+    {
+      path: '/recommend',
+      name: 'Recommend',
+      component: Recommend
+    },
+    {
+      path: '/singer',
+      name: 'Singer',
+      component: Singer
+    },
+    {
+      path: '/rank',
+      name: 'Rank',
+      component: Rank
+    },
+    {
+      path: '/search',
+      name: 'Search',
+      component: Search
     }
   ]
 })
