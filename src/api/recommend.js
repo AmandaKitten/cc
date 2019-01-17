c1af801bbec60287acffe6bd42aa6a438b4f7bb5
t00000685 <t00000685@vipshop.com>
2019-01-17 23:32:53 +0800

    轮播图优化切换缓存以及清内存，取推荐列表值

--- src/api/recommend.js
+++ src/api/recommend.js
@@ -13,3 +13,28 @@ export function getRecommend() {
 
   return jsonp(url, data, options)
 }
+
+export function getDiscList() {
+  const url = 'https://c.y.qq.com/splcloud/fcgi-bin/fcg_get_diss_by_tag.fcg'
+
+  const data = Object.assign({}, commonParams, {
+
+    picmid: 1,
+    rnd: Math.random(),
+    g_tk: 5381,
+    loginUin: 0,
+    hostUin: 0,
+    format: 'json',
+    inCharset: 'utf8',
+    outCharset: 'utf-8',
+    notice: 0,
+    platform: 'yqq.json',
+    needNewCode: 0,
+    categoryId: 10000000,
+    sortId: 5,
+    sin: 0,
+    ein: 29
+  })
+
+  return jsonp(url, data, options)
+}
--- src/base/slider/slider.vue
+++ src/base/slider/slider.vue
@@ -105,6 +105,9 @@ export default {
       this.timer = setTimeout(() => {
         this.slider.goToPage(pageIndex, 0, 400)
       }, this.interval)
+    },
+    destroyed() {
+      clearTimeout(this.timer)
     }
   }
 }
